45ff984d372a3119e6c2fca2177a416bc0753f22
maripiero23 <mari.pieropan@gmail.com>
2022-12-01 15:57:27 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  export default{
+    data (){
+      return{
+        messaggio: "Bella vecchio"
+      }
+    }
+
+  }
+</script>
+
+<template>
+  <h1>{{messaggio}}</h1>
+  <p>Ecco il mio progetto in Vit</p>
+ 
+</template>
+
+<style scoped>
+
+</style>
